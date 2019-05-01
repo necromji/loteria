3f5aba50dea01171ae59af7f4e205fa85b1cbc34
javier calvo <javierignacio.calvojuan@gmail.com>
2019-05-01 10:30:08 +0200

    initial commit

---
+++ src/test/java/com/calvo/loteria/LoteriaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.calvo.loteria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoteriaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
